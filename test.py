6dd1b69a057c3b257ebc55d1d4264076bc41917a
Jayy1287 <sanjayrajakumar17@gmail.com>
2024-01-23 12:44:05 +0530

    add test

--- test.py
+++ test.py
@@ -6,5 +6,9 @@ BASE = "http://127.0.0.1:5000/"
 username = 'admin'
 password = 'NicePassword'
 
-response = requests.get(BASE + "video/1", {}, auth=HTTPBasicAuth(username, password))
+#response = requests.get(BASE + "listEvents", {}, auth=HTTPBasicAuth(username, password))
+#response = requests.put(BASE + "video/3?name=dfsf&location=asddf&capacity=123&date=12012024", {}, auth=HTTPBasicAuth(username, password))
+#response = requests.get(BASE + "video/1", {}, auth=HTTPBasicAuth(username, password))
+response = requests.get(BASE + "listAttendees", {}, auth=HTTPBasicAuth(username, password))
+
 print(response.json())
